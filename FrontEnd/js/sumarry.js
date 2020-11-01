b769ea49b076e53ebaafd3f78c94dc092a173738
nickybittencourt <nickytuon75@gmail.com>
2020-10-31 23:56:16 -0300

    working on back end

--- FrontEnd/js/sumarry.js
+++ FrontEnd/js/sumarry.js
@@ -1,11 +1,24 @@
 function updateHighScoreLabel() {
 
     document.getElementById("high-score").innerHTML = localStorage.getItem("highScore")
+
+    document.getElementById("high-score-form").value = localStorage.getItem("highScore")
 }
 
 function restart() {
 
     window.location.href = "./index.html"
+}
+
+function validateForm() {
+    var x = document.forms["submit"]["userName"].value;
+    
+    if (x == "") {
+        alert("Name must be filled out");
+        return false;
+    }
+
+    return true
 }
 
 window.onload = updateHighScoreLabel()
